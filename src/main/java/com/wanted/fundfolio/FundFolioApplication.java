fdc526730ffeff8d4bdbb76a6f97ea6c63e174cc
MyoungJinSeo <sih5673@gmail.com>
2023-11-09 13:01:27 +0900

    Start Setting

---
+++ src/main/java/com/wanted/fundfolio/FundFolioApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.fundfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundFolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundFolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wanted/fundfolio/FundFolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.fundfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundFolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
