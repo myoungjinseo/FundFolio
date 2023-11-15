213e798258f775b5d50ad83d8181a34fbdf26205
MyoungJinSeo <sih5673@gmail.com>
2023-11-15 18:57:24 +0900

    feat : 멤버 권한, 지출 검색 에러 코드 #11

--- src/main/java/com/wanted/fundfolio/global/exception/ErrorCode.java
+++ src/main/java/com/wanted/fundfolio/global/exception/ErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    NON_EXISTENT_MEMBER(HttpStatus.BAD_REQUEST, "존재하지 않는 멤버입니다.");
+    NON_EXISTENT_MEMBER(HttpStatus.NOT_FOUND, "존재하지 않는 멤버입니다."),
+    NON_EXISTENT_EXPENDITURE(HttpStatus.NOT_FOUND, "존재하지 않는 지출입니다."),
+    Authorization_MEMBER(HttpStatus.FORBIDDEN, "권한이 없는 멤버입니다.");   ;
 
     private final HttpStatus httpStatus;
     private final String message;
