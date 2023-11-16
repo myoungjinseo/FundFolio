8b7c0d33b33ba5e3bbb81689bf7a4264fd1a9e44
MyoungJinSeo <sih5673@gmail.com>
2023-11-16 18:52:08 +0900

    feat : 카테고리 타입 추가 #15

--- src/main/java/com/wanted/fundfolio/domain/category/entity/CategoryType.java
+++ src/main/java/com/wanted/fundfolio/domain/category/entity/CategoryType.java
@@ -6,7 +6,13 @@ import lombok.Getter;
 @Getter
 public enum CategoryType {
     FOOD_EXPENSE("식비"),
-    TRAFFIC_EXPENSE("교통비");
+    TRAFFIC_EXPENSE("교통비"),
+    HOUSE_EXPENSE("주거비"),
+    PHONE_EXPENSE("통신비"),
+    SHOP_EXPENSE("쇼핑비"),
+    LEISURE_EXPENSE("여가비"),
+    MEDICAL_EXPENSE("의료비"),
+    OTHER("기타");
 
 
     @JsonValue
